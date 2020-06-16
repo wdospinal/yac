4695cff460a829886fa5b85bf720a7924f1c9949
Wilson <wdospinal@unal.edu.co>
2020-06-16 15:52:40 -0500

    delete: local host reference

--- yac-front/src/constants/config.js
+++ yac-front/src/constants/config.js
@@ -3,6 +3,5 @@
  */
 
 export const URL = 'https://us-central1-yac-meltstudio.cloudfunctions.net';
-export const URL_LOCAL = 'http://localhost:5001/yac-meltstudio/us-central1';
 export const MESSAGE = '/message';
 export const USER = '/user';
